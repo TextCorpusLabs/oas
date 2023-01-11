f9288d43c50fadd2082015cbeab8dee1d53c2f63
Mark Newman <mark@trinetteandmark.com>
2023-01-11 15:56:57 -0500

    extract_articles and stream to csv (basic)

--- src/oas/modes/Metadata.py
+++ src/oas/modes/Metadata.py
@@ -1,3 +1,5 @@
+import typing as t
+from ..dtypes import Extractor
 from ..dtypes import Metadata as settings
 from .. import utils
 
@@ -20,9 +22,19 @@ class Metadata:
             self._settings.dest.unlink()
 
     def run(self) -> None:
+        fields = Metadata._field_selection()
+        field_names = [x for x in fields.keys()]
         source_files = (path for path in utils.list_folder_tar_balls(self._settings.source))
         doc_collections = (utils.list_documents(file) for file in source_files)
         docs = (x for y in doc_collections for x in y)
         docs = utils.progress_overlay(docs, 'Reading document #')
-        xxx = list(docs)
-        
+        articles = utils.extract_articles(docs, fields)
+        articles = utils.stream_csv(self._settings.dest, field_names, articles)
+        for _ in articles:
+            pass
+    
+    @staticmethod
+    def _field_selection() -> t.Dict[str, Extractor]:
+        fields = {}
+        fields['id'] = utils.extract_id
+        return fields
--- src/oas/utils/__init__.py
+++ src/oas/utils/__init__.py
@@ -1,3 +1,6 @@
+from .extract_helper import extract_articles as extract_articles
+from .extract_helper import extract_id as extract_id
 from .fs_helper import list_folder_tar_balls as list_folder_tar_balls
 from .fs_helper import list_documents as list_documents
+from .fs_helper import stream_csv as stream_csv
 from .progress_helper import progress_overlay as progress_overlay
